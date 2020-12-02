da88b794fb546fcba98afb2de750312b36021777
Veda <veda@localhost.localdomain>
2020-12-01 23:12:56 -0800

    clean up files

--- src/routes/subscribe.svelte
+++ src/routes/subscribe.svelte
@@ -2,16 +2,11 @@
 
 </script>
 
-
-
 <svelte:head>
    <title>Subscription Page</title>
-
 </svelte:head>
 
-
 <div class="sender-form-field" data-sender-form-id="ki6idxfi3489ov7p1"></div>
-
 
 <style>
    div{
