10be28120ac2819684beb97ce8614589c2d02286
318nkstate <10473689+318nkstate@users.noreply.github.com>
2020-12-22 16:57:25 -0800

    Added vid component...
    ...style code according to vs code

--- src/routes/shows/[id].svelte
+++ src/routes/shows/[id].svelte
@@ -1,7 +1,7 @@
 <script context="module">
-   import Static from '../../components/static-placeholder.svelte';
-   import axios from 'axios';
-   import {createMarkdown} from "safe-marked";
+   import Static from "../../components/static-placeholder.svelte";
+   import axios from "axios";
+   import { createMarkdown } from "safe-marked";
 
    const marked = createMarkdown();
 
@@ -11,69 +11,36 @@
       const { CMS_APP_API_URL } = session;
       const res = await axios.get(`${CMS_APP_API_URL}/shows/${id}`);
       show = res.data;
-      return { 
+      return {
          show,
-         CMS_APP_API_URL 
+         CMS_APP_API_URL,
       };
    }
 </script>
+
 <script>
-   export let show, CMS_APP_API_URL;
+   import VidComponent from "../../components/VidComponent.svelte";
    import ImageModal from "../../components/imageModal.svelte";
+   export let show, CMS_APP_API_URL;
 </script>
 
-
-<svelte:head>
-   <title>{show.venue}</title>
-</svelte:head>
-
-<div id="container">
-   <div id="image">
-      {#if show.poster != null} 
-      <img src={CMS_APP_API_URL + show.poster.formats.medium.url} alt=""/>
-      {:else}
-      <Static/>         
-      {/if}
-   </div>
-   <div id="info">
-      <h1>{show.venue}</h1>
-      <div id="time">
-         <p>YMD: {show.date} @ {show.time}</p>
-      </div>
-   {#if show.excerpt  != null}
-      <p>{show.excerpt}</p>
-   {/if}
-   {#if show.after_show_post != null}
-      <section>{@html marked(show.after_show_post)}</section>
-   {/if}
-   </div>
-   <div id="show-images">
-      {#each show.show_pics as pics}
-         <ImageModal 
-            smallerImage={CMS_APP_API_URL + pics.formats.small.url}
-            largerImage={CMS_APP_API_URL + pics.formats.large.url}
-         />
-      {/each}
-   </div>
-</div>
-
 <style>
-   *{
-		color: white;
-	}
+   * {
+      color: white;
+   }
 
-	h1 {
-		text-align: center;
-		font-size: 2.4em;
-		text-transform: uppercase;
-		font-weight: 700;
-	}
-
-	p {
+   h1 {
       text-align: center;
-		margin: 1em auto;
-	}
-   
+      font-size: 2.4em;
+      text-transform: uppercase;
+      font-weight: 700;
+   }
+
+   p {
+      text-align: center;
+      margin: 1em auto;
+   }
+
    #info {
       display: flex;
       flex-direction: column;
@@ -82,35 +49,37 @@
       margin: 1em 1em;
       margin-top: 5%;
    }
-   #container{
-      display:flex;
+   #container {
+      display: flex;
       flex-direction: column;
-      flex-wrap:wrap;
+      flex-wrap: wrap;
       height: auto;
       align-content: center;
       justify-content: center;
       align-items: center;
       margin: auto;
-      width: 100%
+      width: 100%;
    }
-   #time{
+   #time {
       display: flex;
       flex-direction: row;
-      text-align:center;
+      text-align: center;
       justify-content: center;
-      flex-wrap:wrap;
+      flex-wrap: wrap;
       margin: 0;
    }
-   #time p{
+   #time p {
       margin: 1.5em 0;
       padding: 0 1.5em;
-
    }
-   #image{
+   #image {
       width: 25em;
       margin: 2em 0 0 0;
    }
-   #show-images{
+   img {
+      width: 25em;
+   }
+   #show-images {
       display: flex;
       flex-direction: row;
       flex-wrap: wrap;
@@ -118,17 +87,58 @@
       justify-content: center;
       align-content: center;
    }
-   img{
-      width: 25em;
+   @media (min-width: 480px) {
+      h1 {
+         font-size: 4em;
+      }
    }
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
    @media (max-width: 600px) {
-		img{
+      img {
          width: 100%;
       }
-	}
+   }
 </style>
+
+<svelte:head>
+   <title>{show.venue}</title>
+</svelte:head>
+
+<div id="container">
+   <div id="image">
+      {#if show.poster != null}
+         <img src={CMS_APP_API_URL + show.poster.formats.medium.url} alt="" />
+      {:else}
+         <Static />
+      {/if}
+   </div>
+   <div id="info">
+      <h1>{show.venue}</h1>
+      <div id="time">
+         <p>YMD: {show.date} @ {show.time}</p>
+      </div>
+      {#if show.excerpt != null}
+         <p>{show.excerpt}</p>
+      {/if}
+      {#if show.after_show_post != null}
+         <section>
+            {@html marked(show.after_show_post)}
+         </section>
+      {/if}
+   </div>
+   {#if show.show_vids.length != 0}
+      <div id="show-images">
+         {#each show.show_vids as vid}
+            <VidComponent source={CMS_APP_API_URL + vid.url} type={vid.mime} />
+         {/each}
+      </div>
+   {/if}
+   {#if show.show_pics.length != 0}
+      <div id="show-images">
+         {#each show.show_pics as pics}
+            <ImageModal
+               smallerImage={CMS_APP_API_URL + pics.formats.small.url}
+               largerImage={CMS_APP_API_URL + pics.formats.url} />
+         {/each}
+      </div>
+   {/if}
+</div>
