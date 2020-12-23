fb26dfb9af13e13cda7fc277a3e74429ca10a57f
318nkstate <10473689+318nkstate@users.noreply.github.com>
2020-12-22 16:48:59 -0800

    add vid component
    vids for past shows and vids page

---
+++ src/components/VidComponent.svelte
@@ -0,0 +1,23 @@
+<script>
+   export let source = "";
+   export let type = "";
+</script>
+
+
+<video controls class="vid" preload="metadata">
+   <source src={source} type={type} >
+   <track kind="captions">
+
+</video>
+
+
+<style>
+.vid{
+   margin: 0.25em 0.5em;
+   max-height: 40rem;
+   max-width: 40rem;
+   padding: 0.25em;
+   align-self: center;
+   overflow: none;
+}
+</style>
