dc8be4f99af68d7f578420ab9a734ecdd5b48771
318nkstate <10473689+318nkstate@users.noreply.github.com>
2020-12-22 16:52:32 -0800

    added type and addtional css

--- src/components/VidComponent.svelte
+++ src/components/VidComponent.svelte
@@ -3,21 +3,24 @@
    export let type = "";
 </script>
 
+<style>
+   .vid {
+      margin: 0.25em 0.5em;
+      max-height: 40rem;
+      max-width: 40rem;
+      padding: 0.25em;
+      align-self: center;
+      overflow: none;
+   }
+   @media (max-width: 680px) {
+      .vid {
+         height: auto;
+         width: 90vw;
+      }
+   }
+</style>
 
 <video controls class="vid" preload="metadata">
-   <source src={source} type={type} >
-   <track kind="captions">
-
+   <source src={source} type={type} />
+   <track kind="captions" />
 </video>
-
-
-<style>
-.vid{
-   margin: 0.25em 0.5em;
-   max-height: 40rem;
-   max-width: 40rem;
-   padding: 0.25em;
-   align-self: center;
-   overflow: none;
-}
-</style>
