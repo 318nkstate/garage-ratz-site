e5e4e535f76941c333edaf8e85c7a42a2f6a98e8
318nkstate <10473689+318nkstate@users.noreply.github.com>
2021-02-06 17:16:17 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import helmet from "helmet";
-import { v4 as uuidv4 } from "uuid";
+
 
 const { PORT, NODE_ENV, CMS_APP_API_URL } = process.env;
 const dev = NODE_ENV === 'development';
